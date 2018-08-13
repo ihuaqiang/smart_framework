2c10340495b2d8d07f16519ff65d8797521b0f9f
ihuaqiang <ihuaqiang@163.com>
2018-08-13 10:09:23 +0800

    微型框架

---
+++ src/main/java/com/ihuaqiang/smart_framework/SmartFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.ihuaqiang.smart_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFrameworkApplication.class, args);
+	}
+}

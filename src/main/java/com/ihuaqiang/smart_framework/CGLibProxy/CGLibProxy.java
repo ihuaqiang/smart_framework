2be0a58318455d26a121f9c819366bae54b3d020
ihuaqiang <ihuaqiang@163.com>
2018-08-17 14:49:20 +0800

    动态代理

--- src/main/java/com/ihuaqiang/smart_framework/CGLibProxy/CGLibProxy.java
+++ src/main/java/com/ihuaqiang/smart_framework/CGLibProxy/CGLibProxy.java
@@ -13,27 +13,28 @@ import java.lang.reflect.Method;
  */
 public class CGLibProxy implements MethodInterceptor {
 
-    private Object target;
+    private Object target;  ////业务类对象，供代理方法中进行真正的业务方法调用
 
 
+    //相当于JDK动态代理中的绑定
     public Object getInstance(Object target) {
-        this.target = target;
-        Enhancer enhancer = new Enhancer();
-        enhancer.setCallback(this);
-        enhancer.setSuperclass(this.target.getClass());
-        return enhancer.create();
+        this.target = target; //给业务对象赋值
+        Enhancer enhancer = new Enhancer(); //创建加强器，用来创建动态代理类
+        enhancer.setCallback(this);  //设置回调：对于代理类上所有方法的调用，都会调用CallBack，而Callback则需要实现intercept()方法进行拦
+        enhancer.setSuperclass(this.target.getClass());   //为加强器指定要代理的业务类（即：为下面生成的代理类指定父类）
+        return enhancer.create();   // 创建动态代理类对象并返回
     }
 
 
+    // 实现回调方法
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         before();
-        Object result = methodProxy.invokeSuper(o, objects);
+        Object result = methodProxy.invokeSuper(o, objects);  //调用业务类（父类中）的方法
         after();
         return result;
     }
-
 
 
     private void before() {
--- src/main/java/com/ihuaqiang/smart_framework/dynamicproxy/DynamicProxy.java
+++ src/main/java/com/ihuaqiang/smart_framework/dynamicproxy/DynamicProxy.java
@@ -12,17 +12,37 @@ import java.lang.reflect.Proxy;
 public class DynamicProxy implements InvocationHandler {
 
 
-    private Object target;  //被代理的目标对象
+    private Object target;  //业务类实现对象，用来调用具体的业务方法
+
+
+    /*
+     * 绑定业务对象并返回一个代理类
+     * @param target
+     * @return
+     */
 
     public Object bind(Object target) {
-        this.target = target;
+        this.target = target;  //接收业务实现类对象参数
+
+
+        //通过反射机制，创建一个代理类对象实例并返回。用户进行方法调用时使用。
+        //创建代理类对象时，需要传递改业务类的类加载器（用来获取业务实现类的元数据，在包装方法是调用真正的业务方法）
         return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
     }
 
 
+    /**
+     * 包装调用方法：进行预处理，调用后处理
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         before();
+        //调用真正的业务方法
         Object result = method.invoke(target, args);
         after();
         return result;
